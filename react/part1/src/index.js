01d00a425199ca16065ea5f3e685d8c3c2ee14f8
zankoo <zhuqi1101@gmail.com>
2020-07-10 04:18:15 +0800

    1.14

--- react/part1/src/index.js
+++ react/part1/src/index.js
@@ -1,74 +1,66 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const Statistic = ({ text, value }) => (
-  <tr>
-    <td>{text}</td>
-    <td>{value}</td>
-  </tr>
-);
+const anecdotes = [
+  "If it hurts, do it more often",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+];
 
-const Statistics = ({ good, neutral, bad }) => {
-  const all = good + neutral + bad;
-  if (good === 0 && neutral === 0 && bad === 0) {
-    return (
-      <>
-        <h1>Statistics</h1>
-        <p>No feedback given</p>
-      </>
-    );
-  }
-  return (
-    <>
-      <h1>Statistics</h1>
-      <table>
-        <tbody>
-          <Statistic text="good" value={good} />
-          <Statistic text="neutral" value={neutral} />
-          <Statistic text="bad" value={bad} />
-          <Statistic text="all" value={all} />
-          <Statistic text="average" value={((good - bad) / all).toFixed(1)} />
-          <Statistic
-            text="positive"
-            value={((good / all) * 100).toFixed(1) + "%"}
-          />
-        </tbody>
-      </table>
-    </>
+const App = (props) => {
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(
+    Array.apply(null, new Array(6)).map(Number.prototype.valueOf, 0)
   );
-};
 
-const Button = ({ handelClick, text }) => (
-  <button onClick={handelClick}>{text}</button>
-);
+  function randomNum(minNum, maxNum) {
+    switch (arguments.length) {
+      case 1:
+        return parseInt(Math.random() * minNum + 1, 10);
+      case 2:
+        return parseInt(Math.random() * (maxNum - minNum + 1) + minNum, 10);
+      default:
+        return 0;
+    }
+  }
 
-const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  const goodB = () => {
-    setGood(good + 1);
+  const random = () => {
+    let temp;
+    while (true) {
+      temp = randomNum(0, 5);
+      if (temp !== selected) break;
+    }
+    setSelected(temp);
+    console.log(indexOfMax);
   };
 
-  const neutralB = () => {
-    setNeutral(neutral + 1);
+  const vote = () => {
+    const copy = [...votes];
+    copy[selected] += 1;
+    setVotes(copy);
   };
 
-  const badB = () => {
-    setBad(bad + 1);
-  };
+  let indexOfMax = 0;
+  let max = votes.reduce(
+    (votes, c, i) => (c > votes ? ((indexOfMax = i), c) : votes),
+    0
+  );
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <Button handelClick={goodB} text="good" />
-      <Button handelClick={neutralB} text="neutral" />
-      <Button handelClick={badB} text="bad" />
-      <Statistics bad={bad} neutral={neutral} good={good} />
+      <h1>Anecdote of the day</h1>
+      <p>{props.anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={vote}>vote</button>
+      <button onClick={random}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <p>{props.anecdotes[indexOfMax]}</p>
+      <p>has {max} votes</p>
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById("root"));
