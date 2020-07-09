c6d10237defecf5d8948131ce8d02047019d1d64
zankoo <zhuqi1101@gmail.com>
2020-07-10 02:52:01 +0800

    1.11

--- react/part1/src/index.js
+++ react/part1/src/index.js
@@ -1,17 +1,39 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const Statistic = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
+
 const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad;
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return (
+      <>
+        <h1>Statistics</h1>
+        <p>No feedback given</p>
+      </>
+    );
+  }
   return (
     <>
       <h1>Statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {(good - bad) / all}</p>
-      <p>positive {(good / all) * 100 + "%"}</p>
+      <table>
+        <tbody>
+          <Statistic text="good" value={good} />
+          <Statistic text="neutral" value={neutral} />
+          <Statistic text="bad" value={bad} />
+          <Statistic text="all" value={all} />
+          <Statistic text="average" value={((good - bad) / all).toFixed(1)} />
+          <Statistic
+            text="positive"
+            value={((good / all) * 100).toFixed(1) + "%"}
+          />
+        </tbody>
+      </table>
     </>
   );
 };
